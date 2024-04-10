76df1b2b52d9cece65143251e0adeae9da3450e1
dimaslanjaka <dimaslanjaka@gmail.com>
2024-04-10 23:54:25 +0700

    add snackbar

--- assets/js/heading-links.js
+++ assets/js/heading-links.js
@@ -3,7 +3,7 @@ const linkContent = `<i class="fa-duotone fa-link" style="margin-right: 0.2em;">
 for (const heading of headings) {
 	// 3
 	const linkIcon = document.createElement("a"); // 4
-  const canonicalUrl = window.location.href.split('#')[0];
+	const canonicalUrl = window.location.href.split("#")[0];
 	linkIcon.setAttribute("href", `${canonicalUrl}#${heading.id}`); // 5
 	linkIcon.innerHTML = linkContent; // 6
 	// Insert the link icon before the heading text
@@ -11,7 +11,7 @@ for (const heading of headings) {
 
 	// Add click event listener to copy the href to clipboard
 	linkIcon.addEventListener("click", function (event) {
-		event.preventDefault(); // Prevent the default action of the link
+		// event.preventDefault(); // Prevent the default action of the link
 		const href = linkIcon.getAttribute("href"); // Get the href attribute
 
 		// Check if Clipboard API is available
@@ -23,12 +23,22 @@ for (const heading of headings) {
 	});
 }
 
+// Function to display snackbar toast
+function showSnackbar(message) {
+	const snackbar = document.getElementById("snackbar");
+	snackbar.textContent = message;
+	snackbar.className = "snackbar show";
+	setTimeout(function () {
+		snackbar.className = snackbar.className.replace("show", "");
+	}, 3000);
+}
+
 // Function to copy text to clipboard using Clipboard API
 function copyToClipboard(text) {
 	return navigator.clipboard
 		.writeText(text)
 		.then(() => {
-			console.log("Link copied to clipboard:", text);
+			showSnackbar("Link copied to clipboard");
 			// You can add further visual feedback here if needed
 		})
 		.catch((error) => {
@@ -46,6 +56,6 @@ function copyToClipboardFallback(text) {
 	document.execCommand("copy");
 	document.body.removeChild(textArea);
 
-	console.log("[fallback] Link copied to clipboard:", text);
+	showSnackbar("[fallback] Link copied to clipboard");
 	// You can add further visual feedback here if needed
 }
